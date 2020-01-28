4e56e3b5d88d246971ca92c0c4efe34b094055c9
Riley Bowling <rbowling@bsd220.org>
2020-01-28 10:42:41 -0600

    Initial Commit

---
+++ top stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  top stories
+//
+//  Created by Riley Bowling on 1/28/20.
+//  Copyright © 2020 Riley Bowling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
